26c35e2f496b621695ba8777e316e18642dbcd24
Nate Harris <nharris@easypost.com>
2021-12-07 00:56:14 -0700

    Fix README example

--- objectrest/__init__.py
+++ objectrest/__init__.py
@@ -1,1 +1,4 @@
-from objectrest.request_handler import ApiTokenRequestHandler, OAuth2RequestHandler
+from objectrest.request_handler import get, head, options, post, put, patch, delete, \
+    get_json, post_json, put_json, patch_json, delete_json, \
+    get_object, post_object, put_object, patch_object, delete_object, \
+    RequestHandler, ApiTokenRequestHandler, OAuth2RequestHandler
