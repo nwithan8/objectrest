e848392250f6f3aa8806e42dd2971056e5d88b75
Nate Harris <nharris@easypost.com>
2021-12-07 01:06:47 -0700

    Added __all__ for imports

--- objectrest/__init__.py
+++ objectrest/__init__.py
@@ -2,3 +2,26 @@ from objectrest.request_handler import get, head, options, post, put, patch, del
     get_json, post_json, put_json, patch_json, delete_json, \
     get_object, post_object, put_object, patch_object, delete_object, \
     RequestHandler, ApiTokenRequestHandler, OAuth2RequestHandler
+
+__all__ = [
+    'get',
+    'head',
+    'options',
+    'post',
+    'put',
+    'patch',
+    'delete',
+    'get_json',
+    'post_json',
+    'put_json',
+    'patch_json',
+    'delete_json',
+    'get_object',
+    'post_object',
+    'put_object',
+    'patch_object',
+    'delete_object',
+    'RequestHandler',
+    'ApiTokenRequestHandler',
+    'OAuth2RequestHandler'
+]
