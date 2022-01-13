dd440ac8cee4b4200d7579dbcf9733c5f024eb5c
Nate Harris <nharris@easypost.com>
2022-01-13 01:29:22 -0700

    - Bump version to 0.4.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "Copyright © 2021 - Nate Harris"
 author = 'Nate Harris'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.1'
+release = '0.4.0'
 
 
 # -- General configuration ---------------------------------------------------
